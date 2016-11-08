64708a4d37b21480640daeb6e0bed05e2b362c8d
Mark093 <marco.lm993@gmail.com>
2016-11-08 12:17:45 +0100

    Commit again

--- src/HTMLParser.java
+++ src/HTMLParser.java
@@ -6,6 +6,7 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Tag;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
